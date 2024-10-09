8dcb72f7975a9835cdea2203c3dcd7d12a9b9bd0
Maka-77x <mmparrett90+study@gmail.com>
2024-10-09 18:43:03 +0100

    aaah

--- nixos-configurations/desktop/hardware-configuration.nix
+++ nixos-configurations/desktop/hardware-configuration.nix
@@ -61,11 +61,12 @@
 
   ];
 
-  boot.loader.grub = {
-    device = "nodev";
-    efiSupport = true;
+  boot.loader = {
+    grub = {
+      device = "nodev";
+      efiSupport = true;
+    };
   };
-
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
